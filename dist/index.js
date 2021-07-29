bebec65444d55ad8c2376d38f0ce464fa3612104
Oguzhan Ozdemir <aoozdemir@live.com>
2021-07-29 18:57:52 +0300

    Use warning level instead for the logs

--- dist/index.js
+++ dist/index.js
@@ -132,13 +132,13 @@ const command = core.getInput('command');
 const instrumenter_version = core.getInput('instrumenter_version');
 const agent_version = core.getInput('agent_version');
 if (!apikey) {
-    core.error('Thundra API Key is not present. Exiting early...');
-    core.error('Instrumentation failed.');
+    core.warning('Thundra API Key is not present. Exiting early...');
+    core.warning('Instrumentation failed.');
     process.exit(core.ExitCode.Success);
 }
 if (!project_id) {
-    core.error('Thundra Project ID is not present. Exiting early...');
-    core.error('Instrumentation failed.');
+    core.warning('Thundra Project ID is not present. Exiting early...');
+    core.warning('Instrumentation failed.');
     process.exit(core.ExitCode.Success);
 }
 // Setting environment variables programmatically
